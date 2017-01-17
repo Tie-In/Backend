9d1276fb76501d767dfedaa84d68d5a4cafcf72c
tanDKY <tan.dooky@gmail.com>
2017-01-17 19:08:42 +0700

    move authen out

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,19 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :null_session
-  include Authenticable
+
+  # Devise methods overwrites
+  def current_user
+    @current_user ||= User.find_by(auth_token: request.headers['Authorization'])
+  end
+
+  def authenticate_with_token!
+    render json: { errors: "Not authenticated" }, status: :unauthorized unless current_user.present?
+  end
+
+  def user_signed_in?
+    current_user.present?
+  end
 
   rescue_from(ActionController::ParameterMissing) do |parameter_missing_exception|
     error = {}
