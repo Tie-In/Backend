dc59ff4e719210c29958a96dbd34c0c150b7e0aa
tanDKY <tan.dooky@gmail.com>
2017-01-17 18:11:20 +0700

    add auto loadpath

--- config/application.rb
+++ config/application.rb
@@ -20,6 +20,9 @@ module Ror
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
 
+    # autoload path
+    config.autoload_paths += %W(#{config.root}/lib)
+
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
 
