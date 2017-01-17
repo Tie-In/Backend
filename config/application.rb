76847dbe9f24234a0deb012aec1eed95f682974f
tanDKY <tan.dooky@gmail.com>
2017-01-17 18:20:21 +0700

    try

--- config/application.rb
+++ config/application.rb
@@ -21,7 +21,7 @@ module Ror
     # config.i18n.default_locale = :de
 
     # autoload path
-    config.autoload_paths += %W(#{config.root}/lib)
+    config.autoload_paths += %W(\#{config.root}/lib)
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
