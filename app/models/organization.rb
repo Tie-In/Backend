37a71c505d6474879e292768c717c8e2e6f28e88
tanDKY <tan.dooky@gmail.com>
2017-01-24 22:05:26 +0700

    remove owner

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -1,6 +1,4 @@
 class Organization < ActiveRecord::Base
-  has_one :user, foreign_key: 'owner_id'
-
   has_many :user_organizations, :dependent => :destroy
   has_many :users, :through => :user_organizations
 
--- db/migrate/20170116024438_create_organization.rb
+++ db/migrate/20170116024438_create_organization.rb
@@ -3,7 +3,6 @@ class CreateOrganization < ActiveRecord::Migration
     create_table :organizations do |t|
       t.string :name, null: false
       t.string :description, default: ""
-      t.references :owner, references: :user, index: true, foreign_key: true
 
       t.timestamps null: false
     end
