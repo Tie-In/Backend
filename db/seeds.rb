5deac0daf4e503a927e10bddae70a76fb64ff51f
tanDKY <tan.dooky@gmail.com>
2017-03-02 15:14:17 +0700

    update seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,10 +10,58 @@ if Rails.env.development?
   # Create user
   User.create([
     {
-      email: "test@test.com",
-      username: "PP",
+      email: "tan@tiein.com",
+      username: "Tan",
       firstname: "Tan",
-      lastname: "Pornsriniyom",
+      lastname: "Test",
+      birth_date: "2011-11-11",
+      phone_number: "12312312"
+    },
+    {
+      email: "neen@tiein.com",
+      username: "Neen",
+      firstname: "Neen",
+      lastname: "Test",
+      birth_date: "2011-11-11",
+      phone_number: "12312312"
+    },
+    {
+      email: "mint@tiein.com",
+      username: "Mint",
+      firstname: "Mint",
+      lastname: "Test",
+      birth_date: "2011-11-11",
+      phone_number: "12312312"
+    },
+    {
+      email: "chao@tiein.com",
+      username: "Chao",
+      firstname: "Chao",
+      lastname: "Test",
+      birth_date: "2011-11-11",
+      phone_number: "12312312"
+    },
+    {
+      email: "pun@tiein.com",
+      username: "PunPun",
+      firstname: "PunPun",
+      lastname: "Test",
+      birth_date: "2011-11-11",
+      phone_number: "12312312"
+    },
+    {
+      email: "win@tiein.com",
+      username: "Win",
+      firstname: "Varis",
+      lastname: "Test",
+      birth_date: "2011-11-11",
+      phone_number: "12312312"
+    },
+    {
+      email: "max@tiein.com",
+      username: "Maxmi",
+      firstname: "Max",
+      lastname: "Test",
       birth_date: "2011-11-11",
       phone_number: "12312312"
     }
