e563f6c940887087374c7ecb413f5c14e2fd668e
tanDKY <tan.dooky@gmail.com>
2017-03-03 15:38:28 +0700

    done user seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,61 +8,69 @@
 if Rails.env.development?
 
   # Create user
-  User.create([
+  User.create!([
     {
       email: "tan@tiein.com",
       username: "Tan",
       firstname: "Tan",
       lastname: "Test",
-      birth_date: "2011-11-11",
+      password: "12341234",
+      birth_date: "11/11/2011",
       phone_number: "12312312"
     },
     {
       email: "neen@tiein.com",
       username: "Neen",
+      password: "12341234",
       firstname: "Neen",
       lastname: "Test",
-      birth_date: "2011-11-11",
+      birth_date: "11/11/2011",
       phone_number: "12312312"
     },
     {
       email: "mint@tiein.com",
       username: "Mint",
+      password: "12341234",
       firstname: "Mint",
       lastname: "Test",
-      birth_date: "2011-11-11",
+      birth_date: "11/11/2011",
       phone_number: "12312312"
     },
     {
       email: "chao@tiein.com",
       username: "Chao",
+      password: "12341234",
       firstname: "Chao",
       lastname: "Test",
-      birth_date: "2011-11-11",
+      birth_date: "11/11/2011",
       phone_number: "12312312"
     },
     {
       email: "pun@tiein.com",
       username: "PunPun",
+      password: "12341234",
       firstname: "PunPun",
       lastname: "Test",
-      birth_date: "2011-11-11",
+      birth_date: "11/11/2011",
       phone_number: "12312312"
     },
     {
       email: "win@tiein.com",
       username: "Win",
+      password: "12341234",
       firstname: "Varis",
       lastname: "Test",
-      birth_date: "2011-11-11",
+      birth_date: "11/11/2011",
       phone_number: "12312312"
     },
     {
       email: "max@tiein.com",
       username: "Maxmi",
+      password: "12341234",
       firstname: "Max",
       lastname: "Test",
-      birth_date: "2011-11-11",
+      birth_date: "11/11/2011",
       phone_number: "12312312"
     }
   ])
+end
